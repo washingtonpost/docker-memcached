ec6eba72ab5a16ec4efcb84cd6db67d591444146
Patrick Cullen <patrick.cullen@washpost.com>
2017-05-22 12:05:52 -0400

    fixed cluster.sh file

--- cloud-compose/templates/cluster.sh
+++ cloud-compose/templates/cluster.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-{% include "system.resolv_conf.sh" %}
-{% include "system.limits_conf.sh" %}
 {% include "cloud.environment.sh" %}
-{% include "secrets.environment.sh" %}
-{% include "mongodb.hugepage.sh" %}
-{% include "mongodb.alias.sh" %}
+{% include "system.limits_conf.sh" %}
 {% include "system.mounts.sh" %}
 {% include "docker.config.sh" %}
 {% include "docker_compose.run.sh" %}
 {% include "system.network_conf.sh" %}
-{% include "datadog.mongodb.sh" %}
+{% include "datadog.memcached.sh" %}
 {% include "pcp.config.sh" %}
